ff9e30d386cdeb685681d06941668a832f3b8c6a
Mehedi Islam Ripon <mehediislamripon@gmail.com>
2021-04-15 16:56:21 +0600

    reset password feature added!

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 // dependencies
+const crypto = require("crypto");
+
 const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
 const User = require("./../models/userModel");
@@ -171,4 +173,35 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
    }
 });
 
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+   // get user based on the token
+   const hashedToken = crypto
+      .createHash("sha256")
+      .update(req.params.token)
+      .digest("hex");
+
+   const user = await User.findOne({
+      PasswordResetToken: hashedToken,
+      passwordResetExpires: { $gt: Date.now() },
+   });
+
+   // if token has not expired, and there is user, set the new password
+   if (!user) {
+      return next(new AppError("Token is invalid or has expired", 400));
+   }
+   user.password = req.body.password;
+   user.passwordConfirm = req.body.passwordConfirm;
+   user.passwordResetToken = undefined;
+   user.passwordResetExpires = undefined;
+   await user.save();
+
+   // update changePasswordAt property for the user
+   const token = signToken(user._id);
+
+   res.status(200).json({
+      status: "success",
+      token,
+   });
+
+   // log the user in, send JET
+});
--- models/userModel.js
+++ models/userModel.js
@@ -43,6 +43,11 @@ const userSchema = new mongoose.Schema({
    passwordChangedAt: Date,
    PasswordResetToken: String,
    passwordResetExpires: Date,
+   active: {
+      type: Boolean,
+      default: true,
+      select: false,
+   },
 });
 
 userSchema.pre("save", async function (next) {
@@ -55,6 +60,13 @@ userSchema.pre("save", async function (next) {
    // Delete passwordConfirm field
    this.passwordConfirm = undefined;
 
+   next();
+});
+
+userSchema.pre("save", function (next) {
+   if (!this.isModified("password") || this.isNew) return next();
+
+   this.passwordChangedAt = Date.now() - 1000;
    next();
 });
 
