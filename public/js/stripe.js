7c988f26ab89bb03693890ac8ac32fb5d92b6a4c
Mehedi Islam Ripon <mehediislamripon@gmail.com>
2021-05-08 16:45:13 +0600

    finally the app is ready to deployment!

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
-const stripe = Stripe('pk_test_51ImxR8SE7SzMFgj1GZwZ33stT6wkMVPNnJQpyd5xXHRys5OBUs5IuncsplrIbVPjgosPoDN1uekoYxh5lbhszFxB00aOac3EAi');
+const stripe = Stripe(
+   'pk_test_51ImxR8SE7SzMFgj1GZwZ33stT6wkMVPNnJQpyd5xXHRys5OBUs5IuncsplrIbVPjgosPoDN1uekoYxh5lbhszFxB00aOac3EAi'
+);
 
 // pk_test_51ImxR8SE7SzMFgj1GZwZ33stT6wkMVPNnJQpyd5xXHRys5OBUs5IuncsplrIbVPjgosPoDN1uekoYxh5lbhszFxB00aOac3EAi
 
@@ -8,7 +10,7 @@ export const bookTour = async (tourId) => {
    try {
       // get the checkout session from API
       const session = await axios(
-         `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+         `/api/v1/bookings/checkout-session/${tourId}`
       );
 
       // create checkout form + charge credit card
